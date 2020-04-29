700c913ae3c1af77df44c618fd37ce533bda8bb1
Christy Hauschild Schott <62401570+ChristySchott@users.noreply.github.com>
2020-04-29 19:46:44 -0300

    Update Search.js

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { key, proxy } from '../config';
+
 
 export default class Search {
     constructor(query) {
@@ -8,7 +8,7 @@ export default class Search {
 
     async getResults() {
         try {
-            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
             // console.log(this.result);
         } catch (error) {
